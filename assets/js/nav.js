1cae2c593c7a362390c780e6d53c696feaa7343a
Tron <quaat@quasar.net>
2026-03-29 03:46:46 +0200

    Made flowing navbar in phone mode

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,8 +1,14 @@
 const burger = document.querySelector('.hamburger-button');
 const navMenu = document.querySelector('.nav');
+const exitButton = document.querySelector('.nav-exit');
 
 burger.addEventListener('click', () => {
     navMenu.classList.toggle('hidden');
+});
+
+exitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    navMenu.classList.add('hidden');
 });
 
 document.querySelector('header').addEventListener('click', (event) => {
